d96b55e8008c826e61eae1293d2798ecb520a2c4
YuriiBoltukov <kolomiha898@gmail.com>
2023-04-16 09:28:05 +0400

    feat: buttons for add goods

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,20 +1,31 @@
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {Autoplay, EffectFade} from 'swiper';
 import {Item} from '../../interface/interface';
-// import {useDispatch} from 'react-redux';
-// import {addItem} from '../../store/cartSlice';
+import {useDispatch} from 'react-redux';
+import {addItem} from '../../store/cartSlice';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-fade';
 import style from './card.module.scss';
-import {Link} from "react-router-dom";
+import {useState} from "react";
 
 interface CardProps {
     product: Item;
 }
 
 function Card({product}: CardProps) {
-    //const dispatch = useDispatch()
+    const [amount, setAmount] = useState(0)
+    const dispatch = useDispatch()
+
+    function increment():void {
+        setAmount(amount + 1 )
+    }
+
+    function decrement():void {
+        if(amount > 0) {
+            setAmount(amount - 1)
+        }
+    }
 
     return (
         <div className={style.card}>
@@ -44,14 +55,16 @@ function Card({product}: CardProps) {
             </div>
             <h5 className='card-title'>{product.title}</h5>
             <p className='card-text'>{product.price} руб.</p>
-            <button className={style.card_btn}>
-                <Link to='catalog/id'>
-                    Заказать
-                </Link>
+            <div className={style.card_defineNumber}>
+                <button className={style.card_defineNumber_btn} onClick={() => decrement()}>-</button>
+                <input className={style.card_defineNumber_input} type={"number"} value={amount}/>
+                <button className={style.card_defineNumber_btn} onClick={() => increment()}>+</button>
+            </div>
+            <button className={style.card_btn} onClick={() => dispatch(addItem({id: product.id, quantity: 1}))}>
+                Заказать
             </button>
         </div>
     );
 }
 
 export default Card;
-// onClick={() => dispatch(addItem({id: product.id, quantity: 1}))}
